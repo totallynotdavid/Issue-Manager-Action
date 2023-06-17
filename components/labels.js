ca16e8e746cc964eb6d877c0ce5c59a26c2fa724
David Duran <dadch1404@gmail.com>
2023-06-16 21:06:09 -0500

    style: :rotating_light: Lint warnings fixed

--- components/labels.js
+++ components/labels.js
@@ -1,18 +1,18 @@
 async function updateLabels(issue, octokit) {
-  let labelsToAdd = ['documentación'];
+    let labelsToAdd = ['documentación'];
 
-  if (!issue.assignee) {
-    labelsToAdd.push('ayuda');
-  }
+    if (!issue.assignee) {
+        labelsToAdd.push('ayuda');
+    }
 
-  await octokit.issues.update({
-    owner: 'caefisica',
-    repo: 'web',
-    issue_number: issue.number,
-    labels: labelsToAdd,
-  });
+    await octokit.issues.update({
+        owner: 'caefisica',
+        repo: 'web',
+        issue_number: issue.number,
+        labels: labelsToAdd,
+    });
 }
 
 module.exports = {
-  updateLabels,
+    updateLabels,
 };
