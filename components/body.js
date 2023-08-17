f90f6f868793636cb31ab41e541a0291c257a2d4
David Duran <dadch1404@gmail.com>
2023-08-17 05:29:56 -0500

    feat: :sparkles: Passes tests, maybe ready for prod?

--- components/body.js
+++ components/body.js
@@ -10,25 +10,26 @@ async function updateIssue(issue, octokit) {
 
         for (const section of template.body) {
             if (section.type === 'checkboxes') {
-                let tasks = section.attributes.options.map(option => `- [ ] ${option.label}`);
+                const tasks = section.attributes.options.map(option => `- [ ] ${option.label}`);
 
-                const completedTasks = issue.body.match(/- \[x\] .+/gi);
-                if (completedTasks) {
-                    for (const completedTask of completedTasks) {
-                        const taskIndex = tasks.findIndex(task => task.slice(5) === completedTask.slice(5));
-                        if (taskIndex !== -1) {
-                            tasks[taskIndex] = completedTask;
-                        }
+                const completedTasks = issue.body.match(/- \[x\] .+/gi) || [];
+                for (const completedTask of completedTasks) {
+                    const taskIndex = tasks.findIndex(task => task.includes(completedTask.slice(5)));
+                    if (taskIndex !== -1) {
+                        tasks[taskIndex] = completedTask;
                     }
                 }
 
                 newBody += `### ${section.attributes.label}\n\n` + tasks.join('\n') + '\n\n';
             } else if (section.type === 'input') {
-                const sectionContent = issue.body.match(new RegExp(`### ${section.attributes.label}\n\n(.+)\n`));
-                const sectionData = sectionContent ? sectionContent[1] : section.attributes.label;
+                const pattern = new RegExp(`### ${section.attributes.label}\\n\\n([^#]+)`);
+                const sectionContent = issue.body.match(pattern);
+                const sectionData = sectionContent ? sectionContent[1].trim() : section.attributes.label;
                 newBody += `### ${section.attributes.label}\n\n${sectionData}\n\n`;
             }
         }
+
+        newBody = newBody.trim();
 
         await octokit.issues.update({
             owner: 'caefisica',
