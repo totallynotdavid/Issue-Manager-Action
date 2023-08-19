e520a8ef52146310a2f92dae30ea0a3f8eec0955
David Duran <dadch1404@gmail.com>
2023-08-19 11:39:12 -0500

    test: :white_check_mark: Adding & passing tests

--- test-data/expectedBodies.js
+++ test-data/expectedBodies.js
@@ -25,4 +25,108 @@ Termodinámica
 ### Código
 
 CFO604`,
+  `### Pasos
+
+- [ ] Información (nombre, código y créditos)
+- [ ] Descripción del curso (objetivos y contenido)
+- [ ] Prerrequisitos
+- [ ] Temas (mínimo 4)
+- [ ] Libros teóricos (mínimo 5)
+- [ ] Libros teórico-prácticos (mínimo 3)
+- [ ] Libros prácticos y recomendaciones (mínimo 3)
+- [ ] Solucionarios (mínimo 1)
+- [ ] Utilizar el formato LibGen para las tablas
+- [ ] Comentarios y justificación de las recomendaciones de libros
+- [ ] Documentos (mínimo 3)
+- [X] Listas de reproducción (mínimo 5)
+- [ ] Páginas web (mínimo 3)
+- [X] Sílabos
+- [ ] Docentes
+- [ ] Agrupar docentes y sílabos en secciones desplegables (opciones para mostrar y ocultar información)
+
+### Curso
+
+Termodinámica
+
+### Código
+
+CFO604`,
+  `### Pasos
+
+- [ ] Información (nombre, código y créditos)
+- [ ] Descripción del curso (objetivos y contenido)
+- [ ] Prerrequisitos
+- [X] Temas (mínimo 4)
+- [X] Libros teóricos (mínimo 5)
+- [X] Libros teórico-prácticos (mínimo 3)
+- [X] Libros prácticos y recomendaciones (mínimo 3)
+- [X] Solucionarios (mínimo 1)
+- [ ] Utilizar el formato LibGen para las tablas
+- [ ] Comentarios y justificación de las recomendaciones de libros
+- [ ] Documentos (mínimo 3)
+- [X] Listas de reproducción (mínimo 5)
+- [ ] Páginas web (mínimo 3)
+- [X] Sílabos
+- [ ] Docentes
+- [ ] Agrupar docentes y sílabos en secciones desplegables (opciones para mostrar y ocultar información)
+
+### Curso
+
+Termodinámica
+
+### Código
+
+CFO604`,
+  `### Pasos
+
+- [ ] Información (nombre, código y créditos)
+- [ ] Descripción del curso (objetivos y contenido)
+- [ ] Prerrequisitos
+- [X] Temas (mínimo 4)
+- [ ] Libros teóricos (mínimo 5)
+- [ ] Libros teórico-prácticos (mínimo 3)
+- [ ] Libros prácticos y recomendaciones (mínimo 3)
+- [ ] Solucionarios (mínimo 1)
+- [ ] Utilizar el formato LibGen para las tablas
+- [ ] Comentarios y justificación de las recomendaciones de libros
+- [ ] Documentos (mínimo 3)
+- [ ] Listas de reproducción (mínimo 5)
+- [ ] Páginas web (mínimo 3)
+- [ ] Sílabos
+- [ ] Docentes
+- [ ] Agrupar docentes y sílabos en secciones desplegables (opciones para mostrar y ocultar información)
+
+### Curso
+
+Termodinámica
+
+### Código
+
+CFO604`,
+  `### Pasos
+
+- [ ] Información (nombre, código y créditos)
+- [ ] Descripción del curso (objetivos y contenido)
+- [ ] Prerrequisitos
+- [ ] Temas (mínimo 4)
+- [ ] Libros teóricos (mínimo 5)
+- [ ] Libros teórico-prácticos (mínimo 3)
+- [ ] Libros prácticos y recomendaciones (mínimo 3)
+- [ ] Solucionarios (mínimo 1)
+- [ ] Utilizar el formato LibGen para las tablas
+- [ ] Comentarios y justificación de las recomendaciones de libros
+- [ ] Documentos (mínimo 3)
+- [ ] Listas de reproducción (mínimo 5)
+- [ ] Páginas web (mínimo 3)
+- [ ] Sílabos
+- [ ] Docentes
+- [ ] Agrupar docentes y sílabos en secciones desplegables (opciones para mostrar y ocultar información)
+
+### Curso
+
+Termodinámica
+
+### Código
+
+CFO604`
 ];
