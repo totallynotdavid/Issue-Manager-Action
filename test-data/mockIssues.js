1d786e2bce17dd46ade47fd0d187e4b6b5e02d87
David Duran <dadch1404@gmail.com>
2023-08-19 10:56:55 -0500

    test: :white_check_mark: Refactor tests for better modularity

---
+++ test-data/mockIssues.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    number: 123,
+    body: `### Pasos
+
+- [ ] Temas, detallar qué temas se desarrollan en el curso
+- [X] Libros teóricos, junto a comentarios de los motivos de la recomendación
+- [ ] Libros prácticos, junto a comentarios de los motivos de la recomendación
+- [X] Videos, por ejemplo, puede incluir canales de Youtube
+- [ ] Sílabo
+
+### Curso
+
+Termodinámica
+
+### Código
+
+CFO604`
+  }
+]
