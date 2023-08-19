436b4146ad4579868763b77529d67ae40f35a92c
David Duran <dadch1404@gmail.com>
2023-08-19 15:13:46 -0500

    refactor: :recycle: Deleting hardcode values

--- components/body.js
+++ components/body.js
@@ -1,55 +1,65 @@
 const axios = require('axios');
 const yaml = require('js-yaml');
+const config = require('../config');
 
 async function updateIssue(issue, octokit) {
-    try {
-        const response = await axios.get('https://raw.githubusercontent.com/caefisica/web/master/.github/ISSUE_TEMPLATE/plantilla_de_cursos.yml');
-        const template = yaml.load(response.data);
+  try {
+      const response = await axios.get(config.issueTemplateURL);
+      const template = yaml.load(response.data);
 
-        let newBody = '';
+      let newBody = '';
 
-        for (const section of template.body) {
-            if (section.type === 'checkboxes') {
-                const tasks = section.attributes.options.map(option => `- [ ] ${option.label}`);
+      for (const section of template.body) {
+          if (section.type === 'checkboxes') {
+              const templateTasks = section.attributes.options.map(option => `- [ ] ${option.label}`);
+              const issueTasksChecked = issue.body.match(/- \[[xX]\] .+/gi) || [];
+              const issueTasksUnchecked = issue.body.match(/- \[\s\] .+/gi) || [];
 
-                const completedTasks = issue.body.match(/- \[x\] .+/gi) || [];
-                for (const completedTask of completedTasks) {
-                    const taskIndex = tasks.findIndex(task => task.includes(completedTask.slice(5)));
-                    if (taskIndex !== -1) {
-                        tasks[taskIndex] = completedTask;
-                    }
-                }
+              for (let i = 0; i < templateTasks.length; i++) {
+                  const taskLabel = templateTasks[i].slice(5).trim();
 
-                newBody += `### ${section.attributes.label}\n\n` + tasks.join('\n') + '\n\n';
-            } else if (section.type === 'input') {
-                const pattern = new RegExp(`### ${section.attributes.label}\\n\\n([^#]+)`);
-                const sectionContent = issue.body.match(pattern);
-                const sectionData = sectionContent ? sectionContent[1].trim() : section.attributes.label;
-                newBody += `### ${section.attributes.label}\n\n${sectionData}\n\n`;
-            }
-        }
+                  const checkedIndex = issueTasksChecked.findIndex(task => task.trim().includes(taskLabel));
+                  const uncheckedIndex = issueTasksUnchecked.findIndex(task => task.trim().includes(taskLabel));
 
-        newBody = newBody.trim();
+                  if (checkedIndex !== -1) {
+                      templateTasks[i] = issueTasksChecked[checkedIndex];
+                  } else if (uncheckedIndex !== -1) {
+                      templateTasks[i] = issueTasksUnchecked[uncheckedIndex];
+                  }
+              }
 
-        await octokit.issues.update({
-            owner: 'caefisica',
-            repo: 'web',
-            issue_number: issue.number,
-            body: newBody
-        });
+              newBody += `### ${section.attributes.label}\n\n` + templateTasks.join('\n') + '\n\n';
+          } else if (section.type === 'input') {
+              const pattern = new RegExp(`### ${section.attributes.label}\\n\\n([^#]+)`);
+              const sectionContent = issue.body.match(pattern);
+              const sectionData = sectionContent ? sectionContent[1].trim() : section.attributes.label;
+              newBody += `### ${section.attributes.label}\n\n${sectionData}\n\n`;
+          }
+      }
 
-        console.log(`Updated issue #${issue.number}`);
-  
-        await octokit.issues.createComment({
-            owner: 'caefisica',
-            repo: 'web',
-            issue_number: issue.number,
-            body: '¡Hola! Actualizamos este GitHub Issue al formato de nuestra nueva plantilla estandarizada. Esto mejora la gestión y priorización de los problemas. Conservamos tus tareas completadas. Puedes revisar la nueva estructura [aquí](https://github.com/caefisica/web/blob/master/.github/ISSUE_TEMPLATE/plantilla_de_cursos.yml).\n\n<img src="https://media.tenor.com/t8ZbssN1A9kAAAAd/momo-twice.gif" height="250"/>',
-        });
+      newBody = newBody.trim();
 
-    } catch (err) {
-        console.error(`Failed to update issue #${issue.number}`, err);
-    }
+      if (issue.body.trim() !== newBody) {
+          await octokit.issues.update({
+              owner: config.org,
+              repo: config.repo,
+              issue_number: issue.number,
+              body: newBody
+          });
+
+          console.log(`Updated issue #${issue.number}`);
+          await octokit.issues.createComment({
+              owner: config.org,
+              repo: config.repo,
+              issue_number: issue.number,
+              body: '¡Hola! Hemos hecho algunas actualizaciones a este Issue en base a nuestra nueva plantilla estandarizada. Puedes revisar los cambios [aquí](https://github.com/caefisica/web/blob/master/.github/ISSUE_TEMPLATE/plantilla_de_cursos.yml).\n\n<img src="https://media.tenor.com/t8ZbssN1A9kAAAAd/momo-twice.gif" height="250"/>',
+          });
+      } else {
+          console.log(`No changes required for issue #${issue.number}`);
+      }
+  } catch (err) {
+      console.error(`Failed to update issue #${issue.number}`, err);
+  }
 }
 
 module.exports = {
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  org: 'caefisica',
+  repo: 'web',
+  issueTemplateURL: 'https://raw.githubusercontent.com/caefisica/web/master/.github/ISSUE_TEMPLATE/plantilla_de_cursos.yml',
+  issuePrefix: '[📚]:',
+};
