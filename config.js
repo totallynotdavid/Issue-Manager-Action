308ee17a26106e3a2513929f7c939f07990f436c
David Duran <dadch1404@gmail.com>
2023-08-20 01:28:48 -0500

    style: :rotating_light: Fix eslint warnings

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-const core = require("@actions/core");
+const core = require('@actions/core');
 
 module.exports = {
-  org: core.getInput("org"),
-  repo: core.getInput("repo"),
-  branch: core.getInput("branch"),
-  templateName: core.getInput("templateName"),
-  issuePrefix: core.getInput("issuePrefix"),
-  gifURL: core.getInput("gifURL"),
+    org: core.getInput('org'),
+    repo: core.getInput('repo'),
+    branch: core.getInput('branch'),
+    templateName: core.getInput('templateName'),
+    issuePrefix: core.getInput('issuePrefix'),
+    gifURL: core.getInput('gifURL'),
 
-  get issueTemplateURL() {
-      return `https://github.com/${this.org}/${this.repo}/blob/${this.branch}/.github/ISSUE_TEMPLATE/${this.templateName}`;
-  },
-  get rawIssueTemplateURL() {
-      return `${this.issueTemplateURL}?raw=true`;
-  }
+    get issueTemplateURL() {
+        return `https://github.com/${this.org}/${this.repo}/blob/${this.branch}/.github/ISSUE_TEMPLATE/${this.templateName}`;
+    },
+    get rawIssueTemplateURL() {
+        return `${this.issueTemplateURL}?raw=true`;
+    }
 };
