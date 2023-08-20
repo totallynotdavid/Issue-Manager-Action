3534d987aac1a140d3790dcfdfa2e8924c780337
David Duran <dadch1404@gmail.com>
2023-08-19 20:43:25 -0500

    test: :white_check_mark: If no change is needed, the script will automatically handle it

--- test-data/mockIssues.js
+++ test-data/mockIssues.js
@@ -1,6 +1,6 @@
 module.exports = [
+  /* Caso donde las tareas ya no coinciden con las tareas actuales */
   {
-    /* Caso donde las tareas ya no coinciden con las tareas actuales */
     number: 123,
     body: `### Pasos
 
@@ -18,8 +18,8 @@ Termodinámica
 
 CFO604`
   },
+  /* Caso donde algunas tareas coinciden con las tareas actualizadas y están terminadas */
   {
-    /* Caso donde algunas tareas coinciden con las tareas actualizadas y están terminadas */
     number: 1234,
     body: `### Pasos
 
@@ -37,38 +37,8 @@ Termodinámica
 
 CFO604`
   },
+  /* Caso donde solo hay una tarea completada */
   {
-    /* ¡Caso donde una tarea ya fue actualizada antes y no requiere de cambios! */
-    number: 1234,
-    body: `### Pasos
-
-- [ ] Información (nombre, código y créditos)
-- [ ] Descripción del curso (objetivos y contenido)
-- [ ] Prerrequisitos
-- [X] Temas (mínimo 4)
-- [X] Libros teóricos (mínimo 5)
-- [X] Libros teórico-prácticos (mínimo 3)
-- [X] Libros prácticos y recomendaciones (mínimo 3)
-- [X] Solucionarios (mínimo 1)
-- [ ] Utilizar el formato LibGen para las tablas
-- [ ] Comentarios y justificación de las recomendaciones de libros
-- [ ] Documentos (mínimo 3)
-- [X] Listas de reproducción (mínimo 5)
-- [ ] Páginas web (mínimo 3)
-- [X] Sílabos
-- [ ] Docentes
-- [ ] Agrupar docentes y sílabos en secciones desplegables (opciones para mostrar y ocultar información)
-
-### Curso
-
-Termodinámica
-
-### Código
-
-CFO604`
-  },
-  {
-    /* Caso donde solo hay una tarea completada */
     number: 123,
     body: `### Pasos
 
@@ -82,8 +52,8 @@ Termodinámica
 
 CFO604`
   },
+  /* Caso donde solo hay una tarea sin completar */
   {
-    /* Caso donde solo hay una tarea sin completar */
     number: 123,
     body: `### Pasos
 
